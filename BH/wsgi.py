9d36dc806472c293425347d87032e9b54688fd17
hennayasmine19 <hennayasmineshaik@gmail.com>
2025-02-25 13:14:24 +0530

    3

--- BH/wsgi.py
+++ BH/wsgi.py
@@ -1,6 +1,18 @@
-from BH.server import app  # Change 'app' to the correct Flask app instance name if it's different
+import sys
+import os
+
+# Print current directory for debugging
+print("Root directory:", os.getcwd())
+
+# Add the correct paths to the Python path
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'BH'))
+
+# Import your Flask app
+from BH.server import app
+
+# This is required for Vercel
+application = app
 
 if __name__ == "__main__":
     app.run()
-
-application = app
